858737c6f46455c16954d0f1d5a2bcb762e07de9
zinchenkosaymon-commits <zinchenkosaymon@gmail.com>
2026-02-05 18:05:40 +0300

    Add files via upload

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -10,28 +10,35 @@ public class Main {
 
 public static String name = "frogswamp";
 public static JFrame frame = new JFrame(name);
+public static String end = "->";
+public  static JButton buton = new JButton(end);
 public static JTextPane pane = new JTextPane();
 public static JPanel panel = new JPanel();
 public  static JPanel panel1 = new JPanel();
 public static JEditorPane editorPane = new JEditorPane();
 public static JButton button =new JButton("поиск");
+public static JScrollPane pane1 = new JScrollPane(panel1);
     public static void main(String[] args) {
 
         editorPane.setEditable(false);
         pane.setText("найти");
        panel1.setVisible(false);
 panel1.add(editorPane);
+panel1.add(buton);
+
 frame.setSize(400,500);
 frame.setLocationRelativeTo(null);
-frame.setLayout(new FlowLayout(1));
+frame.setLayout(new GridLayout(2,2));
 
 frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 button.addActionListener(new Txt());
+buton.addActionListener(new Txt2());
 panel.add(pane);
 panel.add(button);
+panel1.add(buton);
 
+frame.add(pane1);
 frame.add(panel);
-frame.add(panel1);
 frame.setVisible(true);
     }
 }
--- untitled/src/main/java/org/example/Txt.java
+++ untitled/src/main/java/org/example/Txt.java
@@ -4,7 +4,7 @@ import javax.print.DocFlavor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
-;
+
 
 public class Txt extends Main implements ActionListener {
 
